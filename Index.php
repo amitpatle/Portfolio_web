2a2dfe1c41c6859bd1db4c4bc1c1929799d63d87
A M i T <92509904+amitpatle@users.noreply.github.com>
2023-05-03 13:48:34 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,87 @@
+
+<html>
+<head>
+
+	<title>My Portfolio</title>	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+	<main>		<section id="about">
+
+			<h2>About Me</h2>
+
+			<p>Hello, my name is John Doe and I am a web developer based in New York City.</p>
+
+		</section>
+
+		<section id="projects">
+
+			<h2>My Projects</h2>
+
+			<ul>
+
+				<li>
+
+					<h3>Project 1</h3>
+
+					<p>This is a description of my first project.</p>
+
+				</li>
+
+				<li>
+
+					<h3>Project 2</h3>
+
+					<p>This is a description of my second project.</p>
+
+				</li>
+
+				<li>
+
+					<h3>Project 3</h3>
+
+					<p>This is a description of my third project.</p>
+
+				</li>
+
+			</ul>
+
+		</section>
+
+		<section id="contact">
+
+			<h2>Contact Me</h2>
+
+			<form method="post" action="send-email.php">
+
+				<label for="name">Name:</label>
+
+				<input type="text" name="name" id="name">
+
+				<label for="email">Email:</label>
+
+				<input type="email" name="email" id="email">
+
+				<label for="message">Message:</label>
+
+				<textarea name="message" id="message"></textarea>
+
+				<input type="submit" value="Send">
+
+			</form>
+
+		</section>
+
+	</main>
+  <footer>
+
+		<p>&copy; 2023</p>
+
+	</footer>
+
+</body>
+
+</html>
